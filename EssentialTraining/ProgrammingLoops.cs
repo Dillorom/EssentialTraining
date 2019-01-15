6941a5597ca92c2d9828a99a03afda237bc66cdf
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-15 15:26:47 -0800

    add more different loops

--- EssentialTraining/ProgrammingLoops.cs
+++ EssentialTraining/ProgrammingLoops.cs
@@ -8,6 +8,54 @@ namespace EssentialTraining
 {
     public class ProgrammingLoops
     {
+        public string BreakAndContinue()
+        {
+            var sb = new StringBuilder();
+            var words = new List<string>() { "Bread", "Milk", "Honey", "Cheese", "Apples" };
+            foreach(var word in words)
+            {
+                if (word.StartsWith("M")) continue;
+                if (word.StartsWith("C")) break;
+                sb.AppendLine(word);
+            }
+            return sb.ToString();
+        }
+
+        public int DoWhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            do
+            {
+                sum += counter;
+                counter++;
+            } while (counter < 100);
+            return sum;
+        }
+
+        public int WhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            while(counter < 100)
+            {
+                sum += counter;
+                counter++;
+            }
+            return sum;
+        }
+
+        public int ForEachLoop()
+        {
+            var numbers = new List<int> { 1, 3, 4, 5, 6 };
+            var sum = 0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
         public int ForLoop()
         {
             var sum = 0;
