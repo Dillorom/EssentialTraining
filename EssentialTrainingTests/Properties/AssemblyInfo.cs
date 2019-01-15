278af352b3e2807d74698019b1a63edd83fc04fa
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-15 10:51:46 -0800

    Add project files.

---
+++ EssentialTrainingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTrainingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTrainingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89d8fdeb-ec75-43e5-a4c5-f12c5d84b3b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentialTrainingTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EssentialTraining;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testInstance = new Class1();
+            var testResult = testInstance.AddTwo(5, 9);
+            Assert.AreEqual(14, testResult, "I expect 5 + 9 = 14.");
+
+        }
+    }
+}
