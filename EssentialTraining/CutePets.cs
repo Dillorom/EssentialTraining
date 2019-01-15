59e3fc45514c1f4f37f9353d18e9508afedf9a86
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-15 12:22:13 -0800

    add CutePets List and write a unit test

---
+++ EssentialTraining/CutePets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class CutePets
+    {
+        public List<string> Pets { get; set; }
+
+        public CutePets()
+        {
+            Pets = new List<string>();
+        }
+
+        public bool IsCute(string pet)
+        {
+            return Pets.Contains(pet);
+        }
+    }
+  
+}
---
+++ EssentialTrainingTests/CutePetsTEst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class CutePetsTEst
+    {
+        [TestMethod]
+        public void IsPetCute()
+        {
+            var testInstance = new CutePets();
+            testInstance.Pets.Add("Cat");
+            testInstance.Pets.Add("Puppy");
+            testInstance.Pets.Add("Bunny");
+
+            Assert.IsTrue(testInstance.IsCute("Cat"));
+            Assert.IsFalse(testInstance.IsCute("Raccoon"));
+        }
+    }
+}
