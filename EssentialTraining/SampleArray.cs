a44d7a328d43268bf7d59816d67374d4d3504731
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-15 11:35:50 -0800

    add SampleArray, and write a unit test for it.

---
+++ EssentialTraining/SampleArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class SampleArray
+    {
+        public string[] GroceryList;
+
+        public SampleArray()
+        {
+            GroceryList = new string[4] { "Milk", "Tomatoes", "Bread", "Butter" };
+        }
+
+        public override string ToString()
+        {
+            return "There are + GroceryList.Length + items, and they are + GroceryList.ToString()";
+        }
+    }
+}
---
+++ EssentialTrainingTests/SampleArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SampleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new SampleArray();
+            Assert.AreEqual(testInstance.GroceryList.Length, 4);
+            Assert.AreEqual(testInstance.GroceryList[1], "Tomatoes");
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new SampleArray();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+        }
+    }
+}
